672a51292827d741aa23e006b843723f3c8d5f7c
jzacka1 <jameszacka@gmail.com>
2019-04-01 16:35:29 -0400

    Added two additional tests for checking if 3 returns Fizz, and 5 returns Buzz

--- CustomerMethodTest/UnitTest1.cs
+++ CustomerMethodTest/UnitTest1.cs
@@ -15,6 +15,24 @@ namespace CustomerMethodTest
             strArr = Print100();
         }
 
+        //IF THE NUMBER IS 3, THE RESULT SHOULD BE Fizz.
+        [TestMethod]
+        public void Print100_3_Fizz()
+        {
+            //Index
+            int i = 3;
+            Assert.AreEqual(strArr[i - 1], "3 Fizz");
+        }
+
+        //IF THE NUMBER IS 5, THE RESULT SHOULD BE Buzz.
+        [TestMethod]
+        public void Print100_5_Buzz()
+        {
+            //Index
+            int i = 5;
+            Assert.AreEqual(strArr[i - 1], "5 Buzz");
+        }
+
         //IF THE NUMBER IS 15, THE RESULT SHOULD BE FizzBuzz.
         [TestMethod]
         public void Print100_15_FizzBuzz()
